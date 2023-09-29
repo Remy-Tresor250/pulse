a2c4d358520e87e7e122b192df2ebf5f6160fb79
Remy-Tresor-250 <niyitegekatresor@gmail.com>
2023-09-29 15:31:24 +0200

    initial commit

---
+++ server/pulse/src/main/java/com/bant/pulse/PulseApplication.java
@@ -0,0 +1,13 @@
+package com.bant.pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseApplication.class, args);
+	}
+
+}
---
+++ server/pulse/src/test/java/com/bant/pulse/PulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bant.pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
